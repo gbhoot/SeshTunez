e1fbc1369d4c09e9be375440d43b004483087d31
Gbhoot <gsbhoot1020@gmail.com>
2018-12-24 22:34:31 -0800

    quick update

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var express = require('express'),
 var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(session);
 app.use(express.static(path.join(__dirname, 'static')));
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
